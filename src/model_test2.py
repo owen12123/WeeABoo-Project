4114c4c05327a6536ec621149e4854bcd5b1eded
Raymond So <so_raymond918@hotmail.com>
2017-09-02 10:21:29 -0400

    Modify model test to run on batches of images

--- src/model_test2.py
+++ src/model_test2.py
@@ -6,18 +6,30 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from keras import metrics
 from keras.optimizers import SGD
+import os
 
 import cv2
 import numpy as np
 
 import keras
 from PIL import ImageFile
+import PIL
 
-img_width, img_height = 150, 150
+samplePath = 'ayanami_rei'
 
-img = cv2.imread('test12.jpg')
-img = cv2.resize(img, (150, 150))
-img = np.expand_dims(img, axis=0)
+filenames = []
+for root, dirs, files in os.walk(samplePath):
+    filenames = files
+
+tensor = np.zeros((len(filenames),150,150,3))
+
+for i in range(0,len(filenames)):
+	curr_path = samplePath + '/' + filenames[i]
+	#img = cv2.imread(curr_path)
+	#img = PIL.Image.resize((150, 150))
+	img = PIL.Image.open(curr_path)
+	img = img.resize((150,150))
+	tensor[i] = img
 
 model = keras.models.load_model('100epoch_dratocos_35cls_64bat_softmax.h5')
 
@@ -29,5 +41,9 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 '''
 
-x = model.predict_classes(img)
+print(tensor.shape)
+
+x = model.predict_classes(tensor)
+#for i in range(0,len(filenames)):
+#	print(x[i])
 print(x)
